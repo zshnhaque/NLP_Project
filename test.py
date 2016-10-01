7bb5cdf28f10105d79e4248be450d157369c6d26
zshnhaque <zshnhaque@gmail.com>
2016-10-01 13:46:27 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+import tweepy,time,sys
+
+ckey = 'OzOBF0q8eHA3JR31w0raoeNHs'
+csecret= 'NfUaDrKX0GRDTAJPJcb8LpW62Bf6ayZtyDUzsueejNdGfE8rum'
+atoken = '114016831-WcBD7U5N4XOkwq5rXePcqh3qoe7u0OAhWjJZOqjK'
+asecret = 'BCZlAgupTd2b4Kijj05X8fNBlO2JfOJQLjaV4XSO9SUsk'
